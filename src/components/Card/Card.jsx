bc934aaf8f0231d7550018e0ed4ddb14bfe03415
frankiefab100 <46662771+frankiefab100@users.noreply.github.com>
2022-07-31 23:18:11 +0100

    update card. devtools and youtube resources

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,19 +6,13 @@ const Card = ({ title, coverImage, description, url, index }) => {
   return (
     <div key={index} className="card">
       <div className="card-img">
-        <a href={url}>
+        <a href={url} target="_blank">
           <img src={coverImage} alt="resource image" />
         </a>
       </div>
 
       <div className="card-info">
-        <div className="title-link">
-          <h2>{title} </h2>
-
-          {/* <a className="url-icon" href={url}>
-            <BiLinkExternal />
-          </a> */}
-        </div>
+        <h2 id="title">{title} </h2>
         <p className="description">{description}</p>
       </div>
     </div>
