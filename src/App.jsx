0a903eacb7c86771e93a089570fc43b8e80f5808
frankiefab100 <46662771+frankiefab100@users.noreply.github.com>
2022-11-07 14:51:06 +0100

    feat add route to match non-existing pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import SubmitForm from "./pages/Submit/SubmitForm";
 import Contact from "./pages/Contact/Contact";
+import Error from "./pages/Error/Error";
 import ScrollButton from "./components/ScrollButton/ScrollButton";
 import { Routes, Route } from "react-router-dom";
 
@@ -51,6 +52,7 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
           <Route path="submit" element={<SubmitForm />} />
+          <Route path="*" element={<Error />} />
         </Route>
       </Routes>
       <ScrollButton />
