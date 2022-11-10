f008725034a7f02655f391e613479dbeb87ee50e
frankiefab100 <46662771+frankiefab100@users.noreply.github.com>
2022-11-11 00:17:24 +0100

    feat: add framer-motion filtering animation to card
    This resolves #15

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,20 +1,39 @@
 import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import "./Card.css";
 
 const Card = ({ title, coverImage, description, url, index }) => {
   return (
-    <div key={index} className="card">
-      <div className="card-img">
-        <a href={url} target="_blank">
-          <img src={coverImage} alt="resource image" />
-        </a>
-      </div>
+    <AnimatePresence>
+      <motion.div
+        key={index}
+        className="card"
+        initial={{
+          opacity: 0.1,
+        }}
+        animate={{
+          opacity: 1,
+        }}
+        exit={{
+          opacity: 0,
+        }}
+        transition={{
+          duration: 0.5,
+        }}
+        layout
+      >
+        <div className="card-img">
+          <a href={url} target="_blank">
+            <img src={coverImage} alt="resource image" />
+          </a>
+        </div>
 
-      <div className="card-info">
-        <h3 id="resource-title">{title}</h3>
-        <p className="description detail-text">{description}</p>
-      </div>
-    </div>
+        <div className="card-info">
+          <h3 id="resource-title">{title}</h3>
+          <p className="description detail-text">{description}</p>
+        </div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
