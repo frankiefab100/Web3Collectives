0c3f7311860c8becfc88eba75bb97ab9f7b5114d
frankiefab100 <46662771+frankiefab100@users.noreply.github.com>
2022-11-12 14:28:15 +0100

    feat: adjust navbar and feature margin

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
 import NavigateTop from "./utils/navigateTop";
 import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
