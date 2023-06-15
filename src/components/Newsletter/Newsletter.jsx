c59c608fd49158c31c14d7d8c888deb04807346a
frankiefab100 <46662771+frankiefab100@users.noreply.github.com>
2023-06-15 11:57:55 +0200

    feat: use convertkit for newsletter subscription

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -1,106 +1,146 @@
-import React from "react";
+import { useState } from "react";
 import { FaEnvelope } from "react-icons/fa";
-import { useReducer } from "react";
-import { db } from "../../firebase/firebaseConfig";
-import { addDoc, collection } from "firebase/firestore";
 import "./Newsletter.css";
 
-const initialState = {
-  sendEmail: "",
-  successMessage: false,
-  subscribing: false,
-};
-
-const ACTION = {
-  FIELD: "field",
-  SUCCESS_MESSAGE: "successMessage",
-  SUBSCRIBING: "subscribing",
-  SET_DEFAULT: "setDefault",
-};
-
-function handleEmails(state, action) {
-  switch (action.type) {
-    case ACTION.FIELD: {
-      return {
-        ...state,
-        [action.field]: action.value,
-        sendEmail: action.value,
-      };
-    }
-
-    case ACTION.SUCCESS_MESSAGE: {
-      return {
-        ...state,
-        successMessage: true,
-      };
-    }
-    case ACTION.SUBSCRIBING: {
-      return {
-        ...state,
-        subscribing: true,
-      };
-    }
-    case ACTION.SET_DEFAULT: {
-      return {
-        [action.field]: "",
-        sendEmail: "",
-        subscribing: false,
-        successMessage: false,
-      };
-    }
-    default:
-      throw new Error();
-  }
-}
-
 const Newsletter = () => {
-  const [state, dispatch] = useReducer(handleEmails, initialState);
-  const { sendEmail, successMessage, subscribing } = state;
-  const emailRef = collection(db, "emails");
+  const [email, setEmail] = useState("");
+  const [subscribing, setSubscribing] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  async function handleSubmit(e) {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    await dispatch({ type: ACTION.SUBSCRIBING });
-    await addDoc(emailRef, { email: sendEmail });
-    await dispatch({ type: ACTION.SUCCESS_MESSAGE });
-    await setTimeout(() => {
-      dispatch({ type: ACTION.SET_DEFAULT });
-    }, 3000);
-  }
+    setSubscribing(true);
+    setSuccessMessage("");
+    setErrorMessage("");
+
+    const formId = import.meta.env.VITE_APP_CONVERTKIT_FORM_ID;
+    const url = `https://app.convertkit.com/forms/${formId}/subscriptions`;
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      if (response.ok) {
+        setSuccessMessage("Subscription successful! Thank you");
+        setEmail("");
+      } else {
+        setErrorMessage("Error subscribing. Please try again later.");
+      }
+    } catch (error) {
+      setErrorMessage("An error occurred. Please try again later.");
+    }
+
+    setSubscribing(false);
+  };
+
+  // Inline CSS to overrride default style in Convertkit
+  const emailStyle = {
+    backgroundColor: "var(--bg-color-secondary)",
+    position: "relative",
+    color: "#222",
+    padding: "1.2rem",
+    fontSize: "1rem",
+    width: "550px",
+  };
+
+  const subscribeBtnStyle = {
+    backgroundColor: "var(--cta-primary)",
+    position: "absolute",
+    color: "#fff",
+    cursor: "pointer",
+    borderRadius: "3px",
+    display: "flex",
+    alignItems: "center",
+    fontSize: "80%",
+    gap: "0.6rem",
+    top: "5px",
+    right: "5px",
+    bottom: "5px",
+  };
 
   return (
-    <div className="newsletter-section">
+    <div
+      style={{ display: "flex", flexDirection: "column", textAlign: "center" }}
+    >
       <div className="newsletter-text">
-        <h2 className="newsletter-title">Stay up to date as you learn</h2>
-        <p className="newsletter-subtitle">
+        <h2
+          style={{
+            letterSpacing: "normal",
+            "@media (maxWidth: 560px)": {
+              fontSize: "1.4rem",
+            },
+          }}
+        >
+          Stay up to date as you learn
+        </h2>
+        <p
+          style={{
+            margin: "5px 0 20px",
+            "@media (maxWidth: 560px)": {
+              fontSize: "0.85rem",
+            },
+          }}
+        >
           Sign up to our newsletter for biweekly Web3 resources and insights.
         </p>
       </div>
 
-      <div className="subscribe-section">
-        <div className="newsletter-subscribe">
-          <form id="newsletter-form" onSubmit={handleSubmit}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          textAlign: "center",
+          margin: "10px 50px 0px",
+        }}
+      >
+        <div
+          style={{
+            position: "relative",
+            margin: "1rem 0 3rem",
+          }}
+        >
+          <form
+            style={{
+              position: "relative",
+              display: "block",
+              marginTop: "0em",
+            }}
+            onSubmit={handleSubmit}
+          >
             <input
+              style={{
+                ...emailStyle,
+              }}
               type="email"
               id="email"
               name="email"
               placeholder="Enter your email"
-              onChange={(e) =>
-                dispatch({
-                  type: ACTION.FIELD,
-                  value: e.target.value,
-                  field: "email",
-                })
-              }
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
 
-            <button disabled={subscribing} className="input-cta subscribe-btn">
+            <button
+              disabled={subscribing}
+              style={{
+                ...subscribeBtnStyle,
+              }}
+              id="subscribe-btn"
+              className="input-cta subscribe-btn"
+            >
               Subscribe <FaEnvelope />
             </button>
           </form>
-          {successMessage && "Subscription successful! Thank you"}
+          {successMessage && <p>{successMessage}</p>}
+          {errorMessage && <p>{errorMessage}</p>}
         </div>
       </div>
     </div>
@@ -108,3 +148,84 @@ const Newsletter = () => {
 };
 
 export default Newsletter;
+
+// import { useState } from "react";
+// import { FaEnvelope } from "react-icons/fa";
+
+// const Newsletter = () => {
+//   const [email, setEmail] = useState("");
+//   const [subscribing, setSubscribing] = useState(false);
+//   const [successMessage, setSuccessMessage] = useState("");
+//   const [errorMessage, setErrorMessage] = useState("");
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     setSubscribing(true);
+//     setSuccessMessage("");
+//     setErrorMessage("");
+
+//     const formId = import.meta.env.VITE_APP_CONVERTKIT_FORM_ID;
+//     const url = `https://app.convertkit.com/forms/${formId}/subscriptions`;
+
+//     try {
+//       const response = await fetch(url, {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ email }),
+//       });
+
+//       if (response.ok) {
+//         setSuccessMessage("Subscription successful! Thank you");
+//         setEmail("");
+//       } else {
+//         setErrorMessage("Error subscribing. Please try again later.");
+//       }
+//     } catch (error) {
+//       setErrorMessage("An error occurred. Please try again later.");
+//     }
+
+//     setSubscribing(false);
+//   };
+
+//   return (
+//     <div className="newsletter-section">
+//       <div className="newsletter-text">
+//         <h2 className="newsletter-title">Stay up to date as you learn</h2>
+//         <p className="newsletter-subtitle">
+//           Sign up to our newsletter for biweekly Web3 resources and insights.
+//         </p>
+//       </div>
+
+//       <div className="subscribe-section">
+//         <div className="newsletter-subscribe">
+//           <form id="newsletter-form" onSubmit={handleSubmit}>
+//             <input
+//               type="email"
+//               id="email"
+//               name="email"
+//               placeholder="Enter your email"
+//               value={email}
+//               onChange={(e) => setEmail(e.target.value)}
+//               required
+//             />
+
+//             <button
+//               disabled={subscribing}
+//               id="input-cta"
+//               className="input-cta subscribe-btn"
+//             >
+//               Subscribe <FaEnvelope />
+//             </button>
+//           </form>
+//           {successMessage && <p>{successMessage}</p>}
+//           {errorMessage && <p>{errorMessage}</p>}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Newsletter;
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import NavigateTop from "./utils/navigateTop";
 import { BrowserRouter as Router } from "react-router-dom";
+import "normalize.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
